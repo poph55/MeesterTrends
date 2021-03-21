e717bd8cfa0afb99b838d5bd2efa32ba36013fa2
Palmer Hudson <palmalama@gmail.com>
2021-03-21 01:31:05 -0400

    Quick formatting, input validation

--- meester.py
+++ meester.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='en-US', tz=360)
-
+print("Enter the name of a search term you want to compare to Leighton Meester and Britney Spears:")
 trend = str(input())
 while (trend == 'Leighton Meester' or trend == 'Britney Spears'):
     print("You can't pick that! Try again:")
@@ -21,6 +21,10 @@ data=data[['Britney Spears', 'Leighton Meester', trend]].cumsum()
 brit_avg = data.iloc[-1, 0]
 leight_avg = data.iloc[-1, 1]
 trend_avg = data.iloc[-1, 2]
+
+if (trend_avg < 1):
+    print("Your search term didn't have enough searches to build a dataset :(")
+    exit()
 
 trend_brit = int((round(trend_avg / brit_avg, 2) - 1) * 100)
 brit_trend = int((round(brit_avg / trend_avg, 2) - 1) * 100)
@@ -46,3 +50,4 @@ elif (trend_avg == leight_avg):
 
 elif (trend_avg < leight_avg):
     print("Leighton Meester is " + str(leight_trend) + "% more popular than " + trend + " since 2004.")
+
